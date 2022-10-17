b21c2e34df6989ab1900da59077f08b865eea72f
Andres Orozco <juanxxi2015@gmail.com>
2022-10-17 12:04:10 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Square = (props) => {
+  const [value, setValue] = useState(null);
+  return (
+    <button className="square" 
+    onClick={props.onClickEvent}
+    >
+
+      {props.value}
+    </button>
+  )
+}
+
+const Board = () => {
+  const initialSquares = [
+    null,null,null,
+    null,null,null,
+    null,null,null,
+  ];
+  const [squares, setSquares] =   useState(initialSquares)
+
+
+  const handleClickEvent= (i) =>{
+    alert(`square ${i} clicked`)
+  };
+  const renderSquare = (i) =>{
+    return(
+      <Square value={squares[i]}
+      onClickEvent={() => handleClickEvent(i)}
+      />
+    )
+  }
+  return (
+    <div style={{
+      backgroundColor: "skyblue",
+      margin: 10,
+      padding: 20
+    }
+    }>
+      Board
+      <div className="board-row">
+      {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+      </div>
+      <div className="board-row">
+      {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
+      </div>
+      <div className="board-row">
+      {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+      </div>
+    </div>
+  )
+}
+
+
+const gameStyles = {
+  backgroundColor: 'salmon',
+  margin: 10,
+  padding: 20,
+};
+const Game = () => {
+  return (
+    <div className="game">
+      Game
+      <Board />
+    </div>
+  );
+};
+
+ReactDOM.render(<Game />, document.getElementById('root'))
